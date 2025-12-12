d826402f0dacd23e150019115559c44ac28b66a4
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-12 11:36:51 +0700

    Day 1 - Begineer - Declaring Variables

---
+++ begineer/2-todo-app-cli/exercise/day1-exec-3.py
@@ -0,0 +1,15 @@
+'''
+Your task for this exercise is to create three variables:
+name
+city
+country
+
+and store these values in each variable respectively:
+"John"
+"New York"
+"USA"
+'''
+
+name = "John"
+city = "New York"
+country = "USA"
