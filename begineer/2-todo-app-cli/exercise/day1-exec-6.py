e268f0ac58e1d312418a7abf837a80b59175400e
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-12 15:34:53 +0700

    Day 1 - Begineer - Printing List of Strings

---
+++ begineer/2-todo-app-cli/exercise/day1-exec-6.py
@@ -0,0 +1,9 @@
+'''
+(1) Create list of three strings. The strings can be any text (e.g., "Good Morning", "Dog", etc.)
+(2) associate the list with a variable named words,
+(3) print out the variable/list.
+'''
+
+words = ['Good morning', 'Good afternoon', 'Good evening', 'Good night']
+
+print(words)
