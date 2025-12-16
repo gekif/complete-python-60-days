f27b2c3912f4f5c79d02b65fd16c7f04ccee41ce
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-16 11:40:24 +0700

    Coding Experience - capitalize() vs title()

--- begineer/2-todo-app-cli/main.py
+++ begineer/2-todo-app-cli/main.py
@@ -1,7 +1,8 @@
+user_prompt = "Enter a todo: "
+
 todos = []
 
 while True:
-    user_prompt = "Enter a todo: "
     todo = input(user_prompt)
+    print(todo.title())
     todos.append(todo)
-    print(todos)
