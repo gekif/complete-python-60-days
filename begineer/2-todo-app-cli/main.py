2df1bf0c7c84ae1490bdec660349b3eb025f364c
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-16 11:07:01 +0700

    Building to do app - Getting User Input Repeatedly

--- begineer/2-todo-app-cli/main.py
+++ begineer/2-todo-app-cli/main.py
@@ -1,9 +1,6 @@
 user_prompt = "Enter a todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3, 'Hello']
-print(todos)
-
-print(type(todo1))
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("Next...")
