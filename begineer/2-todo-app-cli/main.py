9ac01b1b3dffa85b1e2eeb816c0d81118e0721d5
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-23 07:54:26 +0700

    Build to do list - cli - Improving the Program Output

--- begineer/2-todo-app-cli/main.py
+++ begineer/2-todo-app-cli/main.py
@@ -2,13 +2,17 @@ todos = []
 
 while True:
     user_action = input("Type add, show or exit: ")
+    user_action = user_action.strip()
 
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo.title())
         case "show":
-            print(todos)
+            for item in todos:
+                print(item)
         case "exit":
             print("Goodbye!")
             break
+        case _:
+            print("Unknown command, please try again.")
