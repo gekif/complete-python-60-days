3ba5562bae08beb1198ed79dd1059d147af44675
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-28 07:38:27 +0700

    Build to do list - cli - Add an Edit Feature

--- begineer/2-todo-app-cli/main.py
+++ begineer/2-todo-app-cli/main.py
@@ -1,7 +1,9 @@
+from hmac import new
+
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -12,6 +14,11 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number -= 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case "exit":
             print("Goodbye!")
             break
