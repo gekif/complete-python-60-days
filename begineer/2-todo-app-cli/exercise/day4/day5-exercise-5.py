a8ea574a1596f35e39ff0ea4c6810126cabe3650
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-28 08:01:57 +0700

    Day 4 - Begineer - Exercise - 5

---
+++ begineer/2-todo-app-cli/exercise/day4/day5-exercise-5.py
@@ -0,0 +1,9 @@
+'''
+(1) add some code that appends the value of current to the end of the list seconds
+Please use the list.append() method to accomplish that.
+'''
+
+seconds = [1.23, 1.45, 1.02]
+current = 1.11
+seconds.append(current)
+print(seconds)
