d22712757953b7d20ac417e569fbcd75c88d8777
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-28 07:43:47 +0700

    Day 4 - Begineer - Exercise - 2

---
+++ begineer/2-todo-app-cli/exercise/day4/day4-exercise-2.py
@@ -0,0 +1,8 @@
+'''
+(1) Define a variable named words,
+(2) Create a list of five items and assign it to the words variable,
+(3) Print out the fourth item of the words list using indexing syntax.
+'''
+
+words = ["apple", "banana", "cherry", "date", "elderberry"]
+print(words[3])
