c53318f40eb6f80ddce7db3e6758d4f8933f99fd
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-28 08:00:30 +0700

    Day 4 - Begineer - Exercise - 4

---
+++ begineer/2-todo-app-cli/exercise/day4/day4-exercise-4.py
@@ -0,0 +1,11 @@
+'''
+(1) iterates over the list of usernames,
+(2) replaces the white space " " of each username with an underscore "_",
+(3) prints out the updated username in each iteration.
+'''
+
+usernames = ['the blueman', 'sorted hedgehog', 'infinite lagoon']
+
+for username in usernames:
+    username = username.replace(" ", "_")
+    print(username)
