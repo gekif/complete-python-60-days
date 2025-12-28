b0045c2e2fa727927b2a22a1ea97f0c00d6b11a5
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-28 08:04:07 +0700

    Day 4 - Begineer - Exercise - 5

---
+++ begineer/2-todo-app-cli/exercise/day4/day4-exercise-5.py
@@ -0,0 +1,13 @@
+'''
+(1) create a color_codes variable and assign a tuple to it,
+(2) the tuple should contain three tuples as items.
+(3) each of the three tuples should contain three items. The items can be any type (e.g,, string, number, etc).
+'''
+
+color_codes = (
+    ('red', '#FF0000', 255),
+    ('green', '#00FF00', 65280),
+    ('blue', '#0000FF', 255)
+)
+
+print(color_codes)
