fd03cd87aa5e10cafbe6ae8bd5633bf0bda1e82f
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-28 07:42:29 +0700

    Day 4 - Begineer - Exercise - 1

---
+++ begineer/2-todo-app-cli/exercise/day4/day4-exercise-1.py
@@ -0,0 +1,4 @@
+# Complete the code so that it prints out the 3rd item of list serials which corresponds to "XYSA9099400".
+
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+print(serials[2])
