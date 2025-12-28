0c406b2136f61fa77fcf9bdc9279c5237611ae64
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-28 08:05:49 +0700

    Day 4 - Begineer - Exercise - 6

---
+++ begineer/2-todo-app-cli/exercise/day4/day5-exercise-6.py
@@ -0,0 +1,10 @@
+'''
+Your task for this exercise is to:
+(1) create three variables mood, strength, and rank,
+(2) assign a string to mood, a float to strength, and an integer to rank.
+(3) the values you assign can be anything as long as they are string types.
+'''
+
+mood = "happy"
+strength = 9.5
+rank = 1
