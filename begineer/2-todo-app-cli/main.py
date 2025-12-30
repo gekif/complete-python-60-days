11443e817ddd7762b828e87ac211a3fd5f806265
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-30 22:22:59 +0700

    Build to do list - cli - Add a Complete Todo Feature

--- begineer/2-todo-app-cli/main.py
+++ begineer/2-todo-app-cli/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,14 +10,17 @@ while True:
             todos.append(todo)
         case "show" | "display":
             for index, item in enumerate(todos):
-                index += 1
                 item = item.title()
-                print(f"{index} - {item}")
+                print(f"{index + 1} - {item}")
         case "edit":
             number = int(input("Number of the todo to edit: "))
             number -= 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            number -= 1
+            todos.pop(number)
         case "exit":
             print("Goodbye!")
             break
