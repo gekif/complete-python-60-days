be669cb61d83d7c41686ddd171c7cd19a87cebe7
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-30 22:24:23 +0700

    Day 5 - Begineer - Exercise - 7

---
+++ begineer/2-todo-app-cli/exercise/day5/day5-exercise-7.py
@@ -0,0 +1,7 @@
+'''
+In the coding area we have a list of floats.
+Add some code that uses a list method to remove item 1.45 from the seconds list.
+'''
+
+seconds = [1.45, 1.30, 1.25, 1.50, 1.40]
+seconds.remove(1.45)
