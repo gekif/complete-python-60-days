d957dac9d43365dbbd0bf8cbe24b766a5271868a
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-30 22:08:14 +0700

    Day 5 - Begineer - Exercise - 6

---
+++ begineer/2-todo-app-cli/exercise/day5/day5-exercise-6.py
@@ -0,0 +1,17 @@
+'''
+Similar to the previous exercise, we have a list of three strings defined in the code area. Your task is to:
+(1) iterate over the filenames list,
+(2) capitalize the first letter of each string,
+(3) add a number and a dash in front of each string, and .txt at the end, and
+(4) print each modified string as shown below.
+0-Document.txt
+1-Report.txt
+2-Presentation.txt
+
+Notice the first letter of each string is capitalized.
+'''
+
+filenames = ["document", "report", "presentation"]
+
+for index, name in enumerate(filenames):
+    print(f"{index}-{name.capitalize()}.txt")
