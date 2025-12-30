2a1d46deba9a6e552b86b6b743db9d77380ce98e
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-30 21:58:22 +0700

    Day 5 - Begineer - Exercise - 2

---
+++ begineer/2-todo-app-cli/exercise/day5/day5-exercise-2.py
@@ -0,0 +1,7 @@
+'''
+(1) assign a list to the rainfallvariable.
+(2) The list should contain four items, a float, an integer, a string, and a list*.
+*It's totally fine for a list to contain another list as an item.
+'''
+
+rainfall = [12.5, 20, "moderate", [5.0, 7.5]]
