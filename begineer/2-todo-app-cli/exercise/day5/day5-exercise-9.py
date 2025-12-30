1934e2367378babf705855337e31a0cce9937095
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-30 22:29:02 +0700

    Day 5 - Begineer - Exercise - 9

---
+++ begineer/2-todo-app-cli/exercise/day5/day5-exercise-9.py
@@ -0,0 +1,16 @@
+'''
+We have defined a list in the coding area. Add some code that:
+(1) iterates over the items of mylist,
+(2) prints out the current length of the list in each iteration
+
+Here is how the output would look like:
+4
+4
+4
+4
+'''
+
+mylist = ['apple', 'banana', 'cherry', 'date']
+
+for item in mylist:
+    print(len(mylist))
