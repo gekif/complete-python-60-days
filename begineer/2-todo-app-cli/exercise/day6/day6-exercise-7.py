361db664e5219e21b59ef14b2c4675bed465e09f
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-02 17:20:32 +0700

    Day 6 - Begineer - Exercise - 7

---
+++ begineer/2-todo-app-cli/exercise/day6/day6-exercise-7.py
@@ -0,0 +1,11 @@
+'''
+In the coding area, we have defined a list of countries. Add some code that uses a for loop to generate a text file for each country (e.g., "Albania.txt", "Belgium.txt", and so on).
+Each file should have its country name as content (e.g., Albania.txt has Albania as content).
+'''
+
+countries = ["Albania", "Belgium", "Canada", "Denmark", "Ethiopia", "France"]
+
+for item in countries:
+    file = open(f"{item}.txt", 'w')
+    file.write(item)
+    file.close()
