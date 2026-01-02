97cdcf57e9dcf0da26eec5f764b7bbf57dea5815
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-02 16:51:11 +0700

    Build to do list - cli - Storing Items in Text Files, Getting Todo Items from Text Files

--- begineer/2-todo-app-cli/main.py
+++ begineer/2-todo-app-cli/main.py
@@ -1,28 +1,50 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
+
     match user_action:
         case "add":
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
-        case "show" | "display":
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+
+        case "show":
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
-                item = item.title()
-                print(f"{index + 1} - {item}")
+                item = item.strip().title()
+                row = f"{index + 1} - {item}"
+                print(row)
+
         case "edit":
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             number = int(input("Number of the todo to edit: "))
             number -= 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+
         case 'complete':
             number = int(input("Number of the todo to complete: "))
             number -= 1
             todos.pop(number)
+
         case "exit":
             print("Goodbye!")
             break
+
         case _:
             print("Unknown command, please try again.")
