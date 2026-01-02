d68587bb0dbcd50340381710a616b561621c6e48
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-02 17:31:59 +0700

    Day 6 - Begineer - Exercise - 9

---
+++ begineer/2-todo-app-cli/exercise/day6/day6-exercise-9.py
@@ -0,0 +1,13 @@
+'''
+filenames = ['doc.txt', 'report.txt', 'presentation.txt']
+Then, create a program that:
+1. generates multiple text files by iterating over the filenames list,
+2. and writes the text Hello  inside each generated text file.
+'''
+
+filenames = ['doc.txt', 'report.txt', 'presentation.txt']
+
+for filename in filenames:
+    file = open(filename, 'w')
+    file.write('Hello ')
+    file.close()
