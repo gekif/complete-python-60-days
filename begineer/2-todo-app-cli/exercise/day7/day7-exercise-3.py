f83f0fd0427484710636cd54e243353f2db08617
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-04 09:54:28 +0700

    Day 7 - Begineer - Exercise - 3

---
+++ begineer/2-todo-app-cli/exercise/day7/day7-exercise-3.py
@@ -0,0 +1,12 @@
+'''
+The coding area contains a list of numbers, each stored as a string. Your task is to add some code that:
+(1) converts the numbers from strings to floats, and
+(2) prints out the updated list.
+
+The output of your code should be as below. Notice the numbers are floats without quotes.
+[10.0, 19.1, 20.0]
+'''
+
+user_entries = ['10', '19.1', '20']
+user_numbers = [float(entry) for entry in user_entries]
+print(user_numbers)
