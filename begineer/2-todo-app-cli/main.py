0a45328337be416e457bdd24ab3d30926a9976d8
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-04 09:43:57 +0700

    Build to do list - cli - Improving code output and adding comments

--- begineer/2-todo-app-cli/main.py
+++ begineer/2-todo-app-cli/main.py
@@ -1,9 +1,11 @@
 while True:
+    # Get use input and strip space chars from it
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
 
     match user_action:
+        # Check if user action is "add"
         case "add":
             todo = input("Enter a todo: ") + "\n"
 
@@ -17,6 +19,7 @@ while True:
             file.writelines(todos)
             file.close()
 
+        # Check if user action is "show"
         case "show":
             file = open("todos.txt", "r")
             todos = file.readlines()
@@ -27,6 +30,7 @@ while True:
                 row = f"{index + 1} - {item}"
                 print(row)
 
+        # Check if user action is "edit"
         case "edit":
             file = open("todos.txt", "r")
             todos = file.readlines()
@@ -37,6 +41,11 @@ while True:
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
 
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+
+        # Check if user action is "complete"
         case 'complete':
             file = open("todos.txt", "r")
             todos = file.readlines()
@@ -44,11 +53,21 @@ while True:
 
             number = int(input("Number of the todo to complete: "))
             number -= 1
-            todos.pop(number)
+            completed_todo = todos.pop(number)
 
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+
+            file = open('complete.txt', 'w')
+            file.writelines(completed_todo)
+            file.close()
+
+        # Check if user action is "exit"
         case "exit":
             print("Goodbye!")
             break
 
+        # Check if user action is "unknown"
         case _:
             print("Unknown command, please try again.")
