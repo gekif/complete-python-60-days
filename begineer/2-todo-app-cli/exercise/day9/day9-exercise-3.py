fca3903668463f00523bf8afd2c6e9d9f6b8181e
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-06 11:35:28 +0700

    Day 9 - Begineer - Exercise - 3

---
+++ begineer/2-todo-app-cli/exercise/day9/day9-exercise-3.py
@@ -0,0 +1,14 @@
+'''
+Your task for this exercise is to:
+(1) assign a dictionary to variable day_temperatures,
+(2) the dictionary should contain three keys: 'morning', 'noon', and 'evening'
+(3) each key should have a float as value.
+'''
+
+day_temperatures = {
+    'morning': 15.5,
+    'noon': 22.3,
+    'evening': 18.7
+}
+
+print(day_temperatures)
