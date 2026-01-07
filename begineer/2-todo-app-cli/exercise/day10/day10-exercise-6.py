8306ebf5cbb684be14c8e3cdd787281a069794d2
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-07 08:03:32 +0700

    Day 10 - Begineer - Exercise - 6

---
+++ begineer/2-todo-app-cli/exercise/day10/day10-exercise-6.py
@@ -0,0 +1,16 @@
+'''
+This exercise is similar to the previous one but with an added feature. In the coding area we have defined a list of filenames. Your task is to:
+(1) loop over the list
+(2) print out the filename of each item without the extension using slicing and with the first letter capitalized.
+
+The output of your program would look like this:
+Report
+Downloads
+Success
+Folders
+'''
+
+filenames = ["report.txt", "downloads.txt", "success.txt", "folders.txt"]
+
+for filename in filenames:
+    print(filename[:-4].capitalize())
