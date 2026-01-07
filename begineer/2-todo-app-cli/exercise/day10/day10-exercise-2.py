d93d822c3b0850934ccb9e7614662210613bacb4
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-07 07:59:00 +0700

    Day 10 - Begineer - Exercise - 2

---
+++ begineer/2-todo-app-cli/exercise/day10/day10-exercise-2.py
@@ -0,0 +1,23 @@
+
+
+
+'''
+As you might know, it is not mathematically possible to divide a number by zero. Consequently, this is also not possible in Python either -you will get a ZeroDivisionError if you try:
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ZeroDivisionError: division by zero
+With that in mind, your task for this exercise is to extend the program you created in Exercise 1 by displaying a message to the user when they enter 0 for the "total value".
+'''
+
+try:
+    total_value = int(input("Enter the total value: "))
+    value = int(input("Enter the value: "))
+
+    percentage = (value / total_value) * 100
+    print(f"That is {percentage}%")
+
+except ValueError:
+    print("You need to enter a number. Run the program again.")
+
+except ZeroDivisionError:
+    print("The total value cannot be zero. Run the program again.")
