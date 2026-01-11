268db7dcc530820bcc49ce90d4461a70e6bd44e2
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-11 07:30:39 +0700

    Day 14 - Begineer - Bugfix - 2

---
+++ begineer/2-todo-app-cli/exercise/day14/day14-bugfix-2/day14-bugfix-2.py
@@ -0,0 +1,16 @@
+"""
+The same program as in exercise 1 now is throwing yet another error. Hunt the error down and fix it.
+
+main.py:
+
+import functions.py
+
+nr_of_periods = functions.count("Trees are good. Grass is green.")
+print(nr_of_periods)
+
+
+functions.py:
+
+def count(phrase):
+    return phrase.count('.')
+"""
