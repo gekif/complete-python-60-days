645aedf448dc4ea34f4007db6cfe3bc89dc5db12
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-14 11:13:34 +0700

    Begineer - To Do Gui - Implementing add button

--- begineer/2-todo-app-gui/main.py
+++ begineer/2-todo-app-gui/main.py
@@ -2,9 +2,32 @@ import todo_functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, button]])
-window.read()
+window = sg.Window(
+    'My To-Do App',
+layout=[
+    [label],
+    [input_box, button]
+],
+    font=('Helvetica', 20)
+)
+
+while True:
+    event, values = window.read()
+
+    #debug
+    print(event)
+    print(values)
+
+    match event:
+        case "Add":
+            todos = todo_functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            todo_functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
