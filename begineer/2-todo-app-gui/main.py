f675fd5aeefffbd3a2f86ae7390e705a5bf4f03a
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-14 16:03:35 +0700

    Begineer - To Do Gui - Implementing edit button

--- begineer/2-todo-app-gui/main.py
+++ begineer/2-todo-app-gui/main.py
@@ -3,13 +3,16 @@ import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-button = sg.Button("Add")
+add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values=todo_functions.get_todos(), key="todos", enable_events=True, size=(45, 10))
 
 window = sg.Window(
     'My To-Do App',
 layout=[
     [label],
-    [input_box, button]
+    [input_box, add_button],
+    [list_box, edit_button]
 ],
     font=('Helvetica', 20)
 )
@@ -20,6 +23,7 @@ while True:
     #debug
     print(event)
     print(values)
+    print(values['todos'])
 
     match event:
         case "Add":
@@ -27,6 +31,30 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             todo_functions.write_todos(todos)
+
+            # 🔥 UPDATE LISTBOX
+            window['todos'].update(values=todos)
+            window['todo'].update('')
+
+
+        case "Edit":
+            todos = todo_functions.get_todos()
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + '\n'
+            todo_functions.write_todos(todos)
+
+            # 🔥 UPDATE LISTBOX
+            window['todos'].update(values=todos)
+            window['todo'].update('')
+
+
+        case "todos":
+            window['todo'].update(values['todos'][0])
+
+
         case sg.WIN_CLOSED:
             break
 
