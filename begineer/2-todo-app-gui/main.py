701be980a08378f13d98f287e02ca3f4f74f6505
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-15 15:31:55 +0700

    Begineer - To Do Gui - Implementing Complete and Exit Button

--- begineer/2-todo-app-gui/main.py
+++ begineer/2-todo-app-gui/main.py
@@ -3,22 +3,22 @@ import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
-edit_button = sg.Button("Edit")
-list_box = sg.Listbox(values=todo_functions.get_todos(), key="todos", enable_events=True, size=(45, 10))
+# add_button = sg.Button("Add")
+# edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values=todo_functions.get_todos(), key="todos", enable_events=True, size=(45,10))
 
-# button_labels = ["Close", "Apply"]
+button_labels = ["Add", "Edit", "Complete", "Delete", "Exit"]
 
-# layout = []
+layout = []
 
-# for bl in button_labels:
-#     layout.append([sg.Button(bl)])
+for bl in button_labels:
+    layout.append([sg.Button(bl)])
 
 layout = [
     [label],
-    [input_box, add_button],
-    [list_box, edit_button]
-    # [bl[0] for bl in layout]
+    [input_box],
+    [list_box],
+    [bl[0] for bl in layout]
 ]
 
 window = sg.Window(
@@ -59,6 +59,30 @@ while True:
             # 🔥 UPDATE LISTBOX
             window['todos'].update(values=todos)
             window['todo'].update('')
+
+        case "Complete":
+            todos = todo_functions.get_todos()
+            todo_to_complete = values['todos'][0]
+            todos.remove(todo_to_complete)
+            todo_functions.write_todos(todos)
+            todo_functions.append_completed(todo_to_complete)
+
+            # 🔥 UPDATE LISTBOX
+            window['todos'].update(values=todos)
+            window['todo'].update('')
+
+        case "Delete":
+            todos = todo_functions.get_todos()
+            todo_to_delete = values['todos'][0]
+            todos.remove(todo_to_delete)
+            todo_functions.write_todos(todos)
+
+            # 🔥 UPDATE LISTBOX
+            window['todos'].update(values=todos)
+            window['todo'].update('')
+
+        case "Exit":
+            break
 
 
         case "todos":
