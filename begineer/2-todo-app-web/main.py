427546826e929e052d30fb1bd939783f7728e8e2
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-16 12:14:43 +0700

    Begineer - Create to do app web - Add new todo items

--- begineer/2-todo-app-web/main.py
+++ begineer/2-todo-app-web/main.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import todo_functions as tf
 
+
 todos = tf.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo.title())
+    tf.write_todos(todos)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
@@ -10,7 +17,7 @@ st.write("This app is to increase my productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new todo...", key="new_todo")
+st.text_input(label="", placeholder="Add a new todo...", on_change=add_todo, key="new_todo")
 
 
 
