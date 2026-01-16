36db5db363564acf76cc58a1d33404f2fc78f6ea
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-16 12:27:55 +0700

    Begineer - Create to do app web - Complete to do items

--- begineer/2-todo-app-web/main.py
+++ begineer/2-todo-app-web/main.py
@@ -8,14 +8,24 @@ def add_todo():
     todo = st.session_state["new_todo"]
     todos.append(todo.title())
     tf.write_todos(todos)
+    st.session_state["new_todo"] = ""
 
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase my productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+
+    if checkbox:
+        tf.append_completed(todo)
+        todos.pop(index)
+        tf.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
 
 st.text_input(label="", placeholder="Add a new todo...", on_change=add_todo, key="new_todo")
 
