a15c93ccaf976e53365eee5a4544c443aa868397
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-19 12:24:10 +0700

    Intermediate - Showcase App - Render the title

--- python-intermediate/3-showcase-website/main.py
+++ python-intermediate/3-showcase-website/main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")
+    st.image("images/photo.png")
 
 with col2:
     st.title("Dzulfikar Maulana")
@@ -19,3 +20,14 @@ Below you can find some of the apps I have built in python. Feel free to contact
 """
 
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
