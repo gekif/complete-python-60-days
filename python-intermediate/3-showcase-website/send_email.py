a55e5b014861ec25e51bb34f25410d30ac25216e
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-20 15:45:55 +0700

    Student Exercise - Best Company - Add Contact Us Form

--- python-intermediate/3-showcase-website/send_email.py
+++ python-intermediate/3-showcase-website/send_email.py
@@ -5,10 +5,10 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "aaa"
-    password = "aaa"
+    username = "app8flask@gmail.com"
+    password = "pmuuyjaxfejfpxkc"
 
-    receiver = "aaa"
+    receiver = username
     context = ssl.create_default_context()
     message = """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
