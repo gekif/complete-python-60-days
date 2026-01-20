066725aa74a2c83bd5e7f2036dee19677e9e3142
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-20 15:38:40 +0700

    Intermediate - Showcase App - Implement send email using webform

--- python-intermediate/3-showcase-website/send_email.py
+++ python-intermediate/3-showcase-website/send_email.py
@@ -1,19 +1,20 @@
-import smtplib
-import ssl
+import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "aaa"
-password = "aaa"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "aaa"
-context = ssl.create_default_context()
-message = """
+    username = "aaa"
+    password = "aaa"
+
+    receiver = "aaa"
+    context = ssl.create_default_context()
+    message = """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 """
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
