7be0995401b1db5aa68cb39a5c225adfcfc8cfbc
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-23 10:41:07 +0700

    Day 25 - Student Exercise - Add Description Below Title

--- python-intermediate/5-excel-to-pdf/exercise/main.py
+++ python-intermediate/5-excel-to-pdf/exercise/main.py
@@ -17,10 +17,15 @@ for filepath in filepaths:
     # and convert it to title case (e.g. Cat)
     filename = Path(filepath).stem
     name = filename.title()
+    desc = Path(filepath).read_text()
 
     # Add the name to the PDF
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=name, ln=1)
+
+    # Add the description to the PDF
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=8, txt=desc)
 
 # Produce the PDF
 pdf.output("output.pdf")
