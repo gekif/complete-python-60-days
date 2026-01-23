4d5fa297ddde1eac6d74c01cf3183517833f17cb
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-23 10:35:46 +0700

    Python Intermediate - Convert Excel to PDF Invoices - Add Header, Total Price, Company and Logo

--- python-intermediate/5-excel-to-pdf/main.py
+++ python-intermediate/5-excel-to-pdf/main.py
@@ -49,6 +49,27 @@ try:
             pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
             pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+        # Total price
+        total_sum = df["total_price"].sum()
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=70, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+        # Add total sum sentence
+        pdf.set_font(family="Times", size=10, style="B")
+        pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+        # Add company name and log
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=25, h=8, txt="Pythonhow")
+        pdf.image("pythonhow.png", w=10)
+
+
+
         pdf.output(f"PDFs/{filename}.pdf")
 
     print("Created PDF")
