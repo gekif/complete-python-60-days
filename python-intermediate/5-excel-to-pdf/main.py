e0fd998a12a738471cc2f0886290e6910c03b25f
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-23 09:36:13 +0700

    Python Intermediate - Convert Excel to PDF Invoices - Add Invoice Date

--- python-intermediate/5-excel-to-pdf/main.py
+++ python-intermediate/5-excel-to-pdf/main.py
@@ -8,16 +8,29 @@ filepaths = glob.glob('Invoices/*.xlsx')
 # Debug print to verify the file paths
 # print(filepaths)
 
-for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
-    pdf.add_page()
-    filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
-    pdf.output(f"PDFs/{filename}.pdf", "F")
+try:
+    for filepath in filepaths:
+        df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
-    # Debug DF
-    print(df)
+        pdf = FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
+
+        filename = Path(filepath).stem
+        invoice_nr, date = filename.split("-")
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Date: {date}")
+
+        pdf.output(f"PDFs/{filename}.pdf", "F")
+
+    print("Created PDF")
+
+        # Debug DF
+        # print(df)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
 
