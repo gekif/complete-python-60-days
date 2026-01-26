ee5ce670a90408e80025cbac901e805e64332b1a
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-26 10:32:19 +0700

    APIS - Weather API - Build REST API

--- apis/7-weather-data-api/main.py
+++ apis/7-weather-data-api/main.py
@@ -1,16 +1,18 @@
+import pandas
 from flask import Flask, render_template, url_for
 
-app = Flask("Website")
+app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/home")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date")
+def about(station, date):
+    df = pandas.read_csv("")
+    temperature = df.station(date)
+    return render_template("about.html", station=station, date=date)
 
 
 if __name__ == "__main__":
