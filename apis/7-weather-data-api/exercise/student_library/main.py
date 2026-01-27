05ba3b1f08a8a5351de77eefe0fa31ab1758130e
Fikar4F <dzulfikar.maulana@gmail.com>
2026-01-27 15:21:55 +0700

    APIS - Weather API - Student Project - Create Thesaurus

--- apis/7-weather-data-api/exercise/student_library/main.py
+++ apis/7-weather-data-api/exercise/student_library/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 
 app = Flask(__name__)
+df = pd.read_csv('dictionary.csv')
 
 
 @app.route('/')
@@ -11,10 +13,20 @@ def home():
 
 @app.route('/api/v1/<word>/')
 def api(word):
-    definition = word.upper()
-    result_dictionary = {'word': word, 'definition': definition}
-    return result_dictionary
+    result = df.loc[df['word'] == word, 'definition']
+
+    if result.empty:
+        return {
+            "error": "Sorry, we don't have this word in our dictionary."
+        }, 404
+
+    definition = result.iloc[0]
+
+    return {
+        'word': word.title(),
+        'definition': definition
+    }
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5001)
