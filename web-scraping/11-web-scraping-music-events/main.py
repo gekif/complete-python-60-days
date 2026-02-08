e79c950dc04bdc636c4fec15861dc03333645266
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-08 10:58:18 +0700

    Web Scraping - Music Events - Storing Extract Data Into Text File

--- web-scraping/11-web-scraping-music-events/main.py
+++ web-scraping/11-web-scraping-music-events/main.py
@@ -13,8 +13,29 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
+def send_email():
+    print("Email was sent")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read():
+    try:
+        with open("data.txt", "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        return ""
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    content = read()
+
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
 
