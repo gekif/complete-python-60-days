2b52b9c645abac3aed86343f58aeda314f82d5ec
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-10 11:00:40 +0700

    OOP - Exercise - Task 1

---
+++ oop/12-oop/exercise/task1.py
@@ -0,0 +1,14 @@
+"""
+Task 1: Create a class named User. The class should:
+1 .have two methods, get_name and age.
+2. The two methods should contain only a pass statement inside them.
+3. The get_name method should contain a self parameter.
+4. The age method should contain a self and a current_year parameter.
+"""
+
+class User:
+    def get_name(self):
+        pass
+
+    def age(self, current_year):
+        pass
