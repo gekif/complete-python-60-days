a7b2751a0e673cd98bfb0515abc3859bf599521f
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-12 10:30:44 +0700

    OOP - Hotel Booking App - Credit Card Feature

--- oop/13-hotel-booking/main.py
+++ oop/13-hotel-booking/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv('hotels.csv', dtype={"id" : str})
+df_cards = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
 
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df['id'] == hotel_id, 'name'].squeeze()
 
     def book(self):
         """Book a hotel by changing its availability to 'no'"""
@@ -21,25 +23,49 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.customer_name.title()}
+        Hotel name: {self.hotel.name.title()}
         """
-        content = f"Name of the customer hotel"
         return content
-        """
-        pass
+
+
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {
+            "number": self.number,
+            "expiration": expiration,
+            "holder": holder,
+            "cvc": cvc
+        }
+        
+        if card_data in df_cards:
+            return True
+        else:
+            return False
 
 
 print(df)
-
 hotel_id = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_id)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard(number="1234")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(name, hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("There was a problem with your payment")
 else:
     print("Hotel is not free.")
