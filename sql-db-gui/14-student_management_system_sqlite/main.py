878a8abbd95133b93b7cbc7818d18cb460317af6
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-16 10:46:42 +0700

    SQLite Student Management System - Add Menu Bar to GUI

--- sql-db-gui/14-student_management_system_sqlite/main.py
+++ sql-db-gui/14-student_management_system_sqlite/main.py
@@ -1,54 +1,24 @@
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtGui import QAction
 import sys
-from datetime import datetime
 
-
-class AgeCalculator(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        grid = QGridLayout()
+        self.setWindowTitle("Student Management System")
 
-        # Create Widgets
-        self.name_label = QLabel("Name:")
-        self.name_line_edit = QLineEdit()
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
 
-        date_birth_label = QLabel("Date of Birth MM/DD/YYYY:")
-        self.date_birth_line_edit = QLineEdit()
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
 
-        calculate_button = QPushButton("Calculate Age")
-        calculate_button.clicked.connect(self.calculate_age)
-
-        self.output_label = QLabel("")
-
-        # Add Widget to Grid
-        grid.addWidget(self.name_label, 0, 0)
-        grid.addWidget(self.name_line_edit, 0, 1)
-        grid.addWidget(date_birth_label, 1, 0)
-        grid.addWidget(self.date_birth_line_edit, 1, 1)
-        grid.addWidget(calculate_button, 2, 0, 1, 2)
-        grid.addWidget(self.output_label, 3, 0, 1, 2)
-
-        self.setLayout(grid)
-        self.setWindowTitle("Age Calculator")
-
-    def calculate_age(self):
-        name = self.name_line_edit.text()
-        date_birth = self.date_birth_line_edit.text()
-
-        try:
-            birth_date = datetime.strptime(date_birth, "%m/%d/%Y")
-            today = datetime.today()
-            age = today.year - birth_date.year - (
-                (today.month, today.day) < (birth_date.month, birth_date.day)
-            )
-
-            self.output_label.setText(f"{name.capitalize()}, you are {age} years old.")
-
-        except ValueError:
-            self.output_label.setText("Invalid date format. Please use MM/DD/YYYY.")
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
 
 
 app = QApplication(sys.argv)
-age_calculator = AgeCalculator()
+age_calculator = MainWindow()
 age_calculator.show()
 sys.exit(app.exec())
