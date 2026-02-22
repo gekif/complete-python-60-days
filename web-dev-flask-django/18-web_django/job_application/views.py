0cee7a9651ca43fadcc9118c341134dc30140a5a
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-23 05:46:52 +0700

    Web Development - Django - Create Email, Fixing Email, Fixing Redirect Cause of Double Data

--- web-dev-flask-django/18-web_django/job_application/views.py
+++ web-dev-flask-django/18-web_django/job_application/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 def index(request):
     if request.method == "POST":
 
-        form = ApplicationForm(request.POST)
+        form = ApplicationForm(request.POST or None)
 
         if form.is_valid():
             first_name = form.cleaned_data["first_name"]
@@ -25,6 +26,16 @@ def index(request):
 
             messages.success(request, "Application submitted successfully!")
 
+            message_body = f"New job application received:\n\nName: {first_name} {last_name}\nEmail: {email}\nDate: {date}\nOccupation: {occupation}"
+            email_message = EmailMessage(
+                subject="New Job Application",
+                body=message_body,
+                to=[email]
+            )
+            email_message.send()
+            return redirect("/")
 
+    else:
+        form = ApplicationForm()
 
-    return render(request, "index.html")
+    return render(request, "index.html", {"form": form})
