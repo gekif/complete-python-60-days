d3c6c059e680ce081610b2e468fb3f64c29fc732
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-24 11:33:04 +0700

    Web Development - Django Restaurant App - Create Context

--- web-dev-flask-django/19-web_restaurant_app_django/restaurant_menu/views.py
+++ web-dev-flask-django/19-web_restaurant_app_django/restaurant_menu/views.py
@@ -6,6 +6,14 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["starters"] = Item.objects.filter(meal_type="starters", status=1).order_by("-date_created")
+        context["salads"] = Item.objects.filter(meal_type="salads", status=1).order_by("-date_created")
+        context["main_dishes"] = Item.objects.filter(meal_type="main_dishes", status=1).order_by("-date_created")
+        context["desserts"] = Item.objects.filter(meal_type="desserts", status=1).order_by("-date_created")
+        return context
+
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
